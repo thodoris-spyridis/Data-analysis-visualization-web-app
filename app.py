b84c6d368b4202b81b22af77f31cfe91a65bb8d2
thodoris-spyridis <thodoris.spyridis@outlook.com>
2023-08-27 18:08:04 +0300

    home page html fixes
    home page html fixes

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, url_for, flash, request, redirect
 from werkzeug.utils import secure_filename
+import datetime
 
 app = Flask(__name__)
+
+today = datetime.date.today()
+current_year = today.year
+print(current_year)
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENSIONS = {"xlsx"}
 
 @app.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template("home.html")
+    return render_template("home.html", year=current_year)
 
 
 if __name__ == "__main__":
