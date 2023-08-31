a0c28d4fa002b6cb3e74d03778be2d14b96af57c
thodoris-spyridis <thodoris.spyridis@outlook.com>
2023-08-31 14:01:22 +0300

    file upload
    file upload

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from flask import Flask, render_template, url_for, flash, request, redirect
 from werkzeug.utils import secure_filename
 import datetime
+import os
+
+UPLOAD_FOLDER = 'temporary_files'
+ALLOWED_EXTENSIONS = {"xlsx"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 today = datetime.date.today()
 current_year = today.year
-print(current_year)
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = {"xlsx"}
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods = ["POST", "GET"])
-def home():
-    return render_template("home.html", year=current_year)
+def upload_file():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("download_file", name=filename))
+    return render_template("upload.html", year=current_year)
+
 
 
 if __name__ == "__main__":
