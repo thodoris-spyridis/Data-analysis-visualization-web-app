3c041d1c1f251de78a48074955ada64fcf8fb1f8
thodoris-spyridis <thodoris.spyridis@outlook.com>
2023-08-31 15:18:24 +0300

    uploading excel files functionality
    uploading excel files functionality added

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 from flask import Flask, render_template, url_for, flash, request, redirect
-from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 import datetime
+from werkzeug.utils import secure_filename
 import os
-
-UPLOAD_FOLDER = 'temporary_files'
-ALLOWED_EXTENSIONS = {"xlsx"}
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileAllowed
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = "datathodoris1988#"
+app.config["UPLOAD_FOLDER"] = "static/files"
+
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired(), FileAllowed(["xlsx"], "wrong format!")])
+    submit = SubmitField("Upload File")
+
 
 today = datetime.date.today()
 current_year = today.year
 
 
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route("/", methods = ["POST", "GET"])
 def upload_file():
-    if request.method == "POST":
-        if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            return redirect(url_for("download_file", name=filename))
-    return render_template("upload.html", year=current_year)
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data  #Grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config["UPLOAD_FOLDER"], secure_filename(file.filename)))  #Find the root directory and save the file after it is validated as secure
+        return "File has been uploaded"
+    return render_template("upload.html", year=current_year, form=form)
 
 
 
