75959f1df03195bdc467bf7461527c5dca7bb144
thodoris-spyridis <thodoris.spyridis@outlook.com>
2023-09-12 23:43:21 +0300

    to delete

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, request, redirect
+from flask import Flask, render_template, url_for, flash, request, redirect, render_template_string
+from flask_plots import Plots
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 import datetime
@@ -9,12 +10,13 @@ from flask_wtf.file import FileAllowed
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 import matplotlib
-from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 
 
 
+
 app = Flask(__name__)
+plots = Plots(app)
 app.config["SECRET_KEY"] = "datathodoris1988#"
 app.config["UPLOAD_FOLDER"] = "static/files"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///file-data.db"
@@ -44,7 +46,7 @@ def clear_files_folder():
 
 @app.route("/", methods=["POST", "GET"])
 def upload_file():
-    global data
+    global my_data
     global file_check
     file_check = False
 
@@ -59,8 +61,8 @@ def upload_file():
             )
         )  # Find the root directory and save the file after it is validated as secure
         file_path = f"static/files/{file.filename}"
-        data = pd.read_excel(file_path, dtype=object)
-        data = data.reset_index(drop=True)
+        my_data = pd.read_excel(file_path, dtype=object)
+        my_data = my_data.reset_index(drop=True)
         os.remove(file_path)
         file_check = True
         flash(f"File {file.filename} has been uploaded")
@@ -70,7 +72,7 @@ def upload_file():
 
 
 @app.route("/no_data", methods=["POST", "GET"])
-def no_data():
+def no_my_data():
     flash("No data available!")
     return render_template("no_data.html")
 
@@ -80,23 +82,22 @@ def get_data():
     if file_check == False:
         return redirect("/no_data")
     else:
-        display_rows = data[0:10]
+        display_rows = my_data[0:10]
         column_names = display_rows.columns
-        row_count = data.shape[0]
-        column_count = data.shape[1]
+        row_count = my_data.shape[0]
+        column_count = my_data.shape[1]
     if request.method == "POST":
-        clear_files_folder()
         column_name = request.form["column-name"]
         action = request.form["action"]
         try:
             if action == "average":
-                result = round(data[column_name].mean(), 2)
+                result = round(my_data[column_name].mean(), 2)
             elif action == "min":
-                result = data[column_name].min()
+                result = my_data[column_name].min()
             elif action == "max":
-                result = data[column_name].max()
+                result = my_data[column_name].max()
             else:
-                unique_values = {value for value in data[column_name].to_list()}
+                unique_values = {value for value in my_data[column_name].to_list()}
                 result = len(unique_values)
         except:
             flash("Action can not be performed on this column")
@@ -107,7 +108,7 @@ def get_data():
         year=current_year,
         preview=display_rows,
         columns=column_names,
-        all_data=data,
+        all_my_data=my_data,
         number_of_rows=row_count,
         number_of_columns=column_count
     )
@@ -115,38 +116,43 @@ def get_data():
 
 @app.route("/visualize", methods=["POST", "GET"])
 def visualize():
-    matplotlib.use("agg")
-    plt.style.use("fivethirtyeight")
-    plot_file = os.path.join("static", "files", "plot.png")
     if file_check == False:
         return redirect("/no_data")
     else:
-        column_names = data.columns
+        column_names = my_data.columns
     if request.method == "POST":
         x_column = request.form["x-axis"]
         y_column = request.form["y-axis"]
-        x_axis = data[x_column].to_list()
-        y_axis = data[y_column].to_list()
+        x_axis = my_data[x_column].to_list()
+        y_axis = my_data[y_column].to_list()
         plot_type = request.form["plot-type"]
         if plot_type == "Linechart":
-            clear_files_folder()
-            plt.plot(x_axis, y_axis)
-            plt.ylabel(y_column)
-            plt.xlabel(x_column)
-            plt.title(f"{y_column} by {x_column} {plot_type}")
-            plt.tight_layout()
-            plt.savefig(plot_file)
-            plt.close()
+            fig = Figure(figsize=(6, 6))
+            ax = fig.subplots()
+            ax = plots.hist(fig, x_axis, y_axis)
+            ax.set_title("Bar chart")
+            data = plots.get_data(fig)
+            return render_template_string(
+            """
+            {% from 'plots/utils.html' import render_img %}
+            {{ render_img(data=data, alt_img='my_img') }}
+            """,   
+            data=data             
+            )
         elif plot_type == "Bar-chart":
-            clear_files_folder()
-            plt.bar(x_axis, y_axis)
-            plt.ylabel(y_column)
-            plt.xlabel(x_column)
-            plt.title(f"{y_column} by {x_column} {plot_type}")
-            plt.tight_layout()
-            plt.savefig(plot_file)
-            plt.close()
-    return render_template("visualize.html", columns=column_names, plot_pic=plot_file)
+            fig = Figure(figsize=(6, 6))
+            ax = fig.subplots()
+            ax = plots.bar(fig, x_axis, y_axis)
+            ax.set_title("Bar chart")
+            data = plots.get_data(fig)
+            return render_template_string(
+            """
+            {% from 'plots/utils.html' import render_img %}
+            {{ render_img(data=data, alt_img='my_img') }}
+            """,   
+            data=data             
+            )
+    return render_template("visualize.html", columns=column_names)
 
 
 
