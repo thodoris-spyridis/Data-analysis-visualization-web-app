537386184b9648e6810d2db8ef70323cfba78136
thodoris-spyridis <thodoris.spyridis@outlook.com>
2024-01-17 13:59:15 +0200

    Removed one metric value

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error
 import matplotlib.pyplot as plt
 
 
@@ -28,9 +28,8 @@ def plot_results(column_list, x_test, y_test, y_pred, plot_file):
     var_score = round(explained_variance_score(y_test, y_pred), 3)
     mean_score = round(mean_absolute_error(y_test, y_pred), 3)
     mean_sqr_score = round(mean_squared_error(y_test, y_pred), 3)
-    r2 = round(r2_score(y_test, y_pred), 3)
     fig, ax = plt.subplots(nrows=1, ncols=len(column_list), constrained_layout=True, figsize=(17,5), facecolor="#e4f1fe")
-    fig.suptitle(f"Explained variance score = {var_score}, Mean absolute score = {mean_score}, Mean squared error = {mean_sqr_score}, R² score = {r2}", fontsize="small")
+    fig.suptitle(f"Explained variance score = {var_score}, Mean absolute score = {mean_score}, Mean squared error = {mean_sqr_score}", fontsize="small")
     for n in range(len(column_list)):
         ax[n].scatter(x=x_test[:, n], y=y_test, marker="o", c="b")
         ax[n].scatter(x=x_test[:, n], y=y_pred, marker="x", c="r")
